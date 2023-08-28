05c1d5c3c00bb6bb1e755305b4fc1be6b606ce0c
Gustavo dos Santos Nascimento <74427958+Gustavo-S-Nascimento@users.noreply.github.com>
2023-08-28 03:35:50 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function showTextBox() {
+  document.getElementById("textBox").style.display = "block";
+}
+function hideTextBox() {
+  document.getElementById("textBox").style.display = "none";
+}
+
+function openForm() {
+  document.getElementById("form").style.display = "block";
+}
